556207b8e72df4541e5f84019cc00849d90a67e9
Dave Welsh <dave@qccareerschool.com>
2023-05-12 16:40:52 -0400

    Add landing pages with telephone number fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
-import type { FC } from 'react';
-import { useId, useRef } from 'react';
+import type { ChangeEventHandler, FC, FormEventHandler } from 'react';
+import { useId, useReducer, useRef } from 'react';
 
 import { getRandomIntInclusive } from '../lib/randomInt';
 import { useGeoLocationContext } from '@/hooks/useGeoLocationContext';
@@ -11,13 +11,68 @@ type Props = {
   buttonClass?: string;
 };
 
+type State = {
+  telephoneNumber: string;
+  smsOptIn: boolean;
+  telephoneError: boolean;
+  telephoneErrorMessage?: string;
+};
+
+type Action =
+  | { type: 'TELEPHONE_NUMBER_CHANGED'; payload: string }
+  | { type: 'SMS_OPT_IN_CHANGED'; payload: boolean };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'TELEPHONE_NUMBER_CHANGED': {
+      let telephoneError = false;
+      let telephoneErrorMessage: string | undefined = undefined;
+      if (state.smsOptIn && action.payload.length === 0) {
+        telephoneError = true;
+        telephoneErrorMessage = 'You have opted into getting text messages. A telephone number is required.';
+      }
+      return { ...state, telephoneNumber: action.payload, telephoneError, telephoneErrorMessage };
+    }
+    case 'SMS_OPT_IN_CHANGED': {
+      let telephoneError = false;
+      let telephoneErrorMessage: string | undefined = undefined;
+      if (state.telephoneNumber.length === 0 && action.payload) {
+        telephoneError = true;
+        telephoneErrorMessage = 'You have opted into getting text messages. A telephone number is required.';
+      }
+      return { ...state, smsOptIn: action.payload, telephoneError, telephoneErrorMessage };
+    }
+  }
+};
+
+const initialState: State = { telephoneNumber: '', smsOptIn: false, telephoneError: false };
+
 export const Form: FC<Props> = ({ action, telephoneNumber = false, buttonText = 'Get the Catalog', buttonClass = 'btn btn-primary' }) => {
   const id = useId();
   const geoLocation = useGeoLocationContext();
   const testGroupId = useRef(getRandomIntInclusive(1, 12));
 
+  const telephoneNumberRef = useRef<HTMLInputElement>(null);
+
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const handleTelephoneNumberChange: ChangeEventHandler<HTMLInputElement> = e => {
+    dispatch({ type: 'TELEPHONE_NUMBER_CHANGED', payload: e.target.value });
+  };
+
+  const handleSMSOptInChange: ChangeEventHandler<HTMLInputElement> = e => {
+    dispatch({ type: 'SMS_OPT_IN_CHANGED', payload: e.target.checked });
+  };
+
+  const handleSubmit: FormEventHandler = e => {
+    if (state.telephoneError) {
+      e.preventDefault();
+      telephoneNumberRef.current?.focus();
+    }
+  };
+
   return (
-    <form id={`form${id}`} method="post" action={action}>
+    <form onSubmit={handleSubmit} id={`form${id}`} method="post" action={action}>
       <input type="hidden" name="school" value="QC Makeup Academy" />
       <input type="hidden" name="testGroup" value={testGroupId.current} />
       <input type="hidden" name="countryCode" value={geoLocation.countryCode} />
@@ -37,7 +92,8 @@ export const Form: FC<Props> = ({ action, telephoneNumber = false, buttonText = 
       {telephoneNumber && (
         <div className="mb-3">
           <label htmlFor={`telephoneNumber${id}`}>Phone Number</label>
-          <input type="tel" name="telephoneNumber" id={`telephoneNumber${id}`} className="form-control" autoCapitalize="off" autoComplete="tel" />
+          <input ref={telephoneNumberRef} onChange={handleTelephoneNumberChange} value={state.telephoneNumber} type="tel" name="telephoneNumber" id={`telephoneNumber${id}`} className={`form-control ${state.telephoneError ? 'is-invalid' : ''}`} autoCapitalize="off" autoComplete="tel" />
+          {state.telephoneErrorMessage && <div className="invalid-feedback">{state.telephoneErrorMessage}</div>}
         </div>
       )}
       <div className="mb-3">
@@ -45,6 +101,14 @@ export const Form: FC<Props> = ({ action, telephoneNumber = false, buttonText = 
           <input type="checkbox" className="form-check-input" id={`emailOptIn${id}`} name="emailOptIn" value="Yes" />
           <label className="form-check-label small fst-italic" htmlFor={`emailOptIn${id}`}>I agree to receive additional emails from QC, including promotions, course launches, special offers and more. Unsubscribe anytime!</label>
         </div>
+        {telephoneNumber && (
+          <div className="mt-2">
+            <div className="form-check">
+              <input onChange={handleSMSOptInChange} checked={state.smsOptIn} type="checkbox" className="form-check-input" id={`smsOptIn${id}`} name="smsOptIn" value="yes" />
+              <label className="form-check-label small fst-italic" htmlFor={`smsOptIn${id}`}>Text me with news and offers.</label>
+            </div>
+          </div>
+        )}
       </div>
       <div className="mt-4">
         <button className={buttonClass}>{buttonText}</button>
