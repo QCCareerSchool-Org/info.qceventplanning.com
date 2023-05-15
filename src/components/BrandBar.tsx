7c7e53a0e1fd5f47baa53113bf5a795d36036262
Dave Welsh <dave@qccareerschool.com>
2023-05-15 13:54:26 -0400

    Use .page.tsx extension

--- src/components/BrandBar.tsx
+++ src/components/BrandBar.tsx
@@ -3,8 +3,12 @@ import type { FC } from 'react';
 
 import Logo from '../images/banner-logo.svg';
 
+import styles from './brandbar.module.css';
+
 export const BrandBar: FC = () => (
-  <div className="bg-black text-white py-3 shadow-4 d-flex justify-content-center">
-    <Image src={Logo} height="24" alt="QC Event Planning" priority />
+  <div className={`${styles.bar} bg-black text-white py-3 shadow-4 d-flex justify-content-center`}>
+    <a href="https://www.qceventplanning.com">
+      <Image src={Logo} height="24" alt="QC Event Planning" priority />
+    </a>
   </div>
 );
--- src/pages/free-floral-design-course-catalog.page.tsx
+++ src/pages/free-floral-design-course-catalog.page.tsx
@@ -11,36 +11,15 @@ import ExpertIcon from '../images/icons/expert-icon.svg';
 import FlexibleIcon from '../images/icons/flexible-icon.svg';
 import HandsOnIcon from '../images/icons/hands-on-icon-2.svg';
 import LifetimeIcon from '../images/icons/lifetime-icon.svg';
+import { testimonialData } from './free-floral-design-course-catalog-testimonials';
 import { About } from '@/components/About';
 import { Card } from '@/components/Card';
 import { Form } from '@/components/Form';
 import { FormWrapper } from '@/components/FormWrapper';
 import { GetStarted } from '@/components/GetStarted';
 import { SEO } from '@/components/SEO';
-import type { TestimonialData } from '@/components/Testimonials';
 import { Testimonials } from '@/components/Testimonials';
 import { useScreenWidthContext } from '@/hooks/useScreenWidthContext';
-
-const testimonialData: TestimonialData[] = [
-  {
-    excerpt: 'I would recommend this course to those looking for a career in the floral industry.',
-    quote: 'I enjoyed the courses I completed previously with QC and Floral Design ended up being no different. The instructions for each assignment were detailed, even down to how to take the pictures to show your work. Feedback from the tutor was helpful and provided the necessary encouragement in order to get the desired results going forward; plus Renee’s tips were quite helpful in navigating my own style. The videos were informative and showed you step by step how to carry out the designs. I have recently started a wedding, event planning plus decor business and hope to begin creating floral designs for clients soon.',
-    name: 'Devona Simons',
-    title: 'Floral Design Graduate',
-  },
-  {
-    excerpt: 'I highly recommend this course to any aspiring florist.',
-    quote: 'My goal is to refresh my skills as a floral professional and learn new techniqes. Upon graduation, I’ll be lucky enough to add a new certification to my resume and website: QC’s International Floral Design Professional(IFDP) designation. This course offers hands-on experience from the comfort of your own home. You’ll also be presented with real-life client scenarios that will help you when you branch out into the industry.',
-    name: 'Neena McConnell',
-    title: 'Floral Design Student',
-  },
-  {
-    excerpt: 'I 100% recommend QC Event School!',
-    quote: 'QC has provided me with an incredible amount of knowledge and mentorship. I was away from school for 14 years and I was a little nervous to go back. QC allowed me the opportunity to create my own curriculum and my own schedule! The Tutors are are always so passionate and supportive when it comes to evaluating my work. The Student Support staff are the rockstars behind the scenes. They are always there to answer my questions. I have been warmly welcomed into this community and I have already talked to some incredibly inspiring people from around the world!',
-    name: 'Carli Lewis',
-    title: 'QC Event School Graduate',
-  },
-];
 
 const FloralDesignCourseCatalogPage: NextPage = () => {
   const screenWidth = useScreenWidthContext();
@@ -71,7 +50,7 @@ const FloralDesignCourseCatalogPage: NextPage = () => {
             <Card>
               <FormWrapper>
                 <h2 className="h5 fw-bold mb-4">Download the Free Course Catalog</h2>
-                <Form action="https://go.qceventplanning.com/l/947642/2023-01-18/tflhv" telephoneNumber />
+                <Form action="https://go.qceventplanning.com/l/947642/2023-01-18/tflhv" />
               </FormWrapper>
             </Card>
           </div>
